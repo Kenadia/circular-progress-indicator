2e7c59ee72b54949a8cc2f1900d8c4cdc77312b9
Ken Schiller <kenschiller@gmail.com>
2015-01-25 22:38:22 -0600

    Draw two arcs and a circle

--- app/js/app.js
+++ app/js/app.js
@@ -4,45 +4,77 @@ angular.module('testApp', [])
 .directive('myCircularIndicator', function () {
 
   var link = function (scope, element, attrs) {
-    var width = attrs.width = scope.width = parseInt(scope.width || 100);
-    var height = attrs.height = scope.height = parseInt(scope.height || scope.width);
     attrs.expected = scope.expected = parseFloat(scope.expected || 0);
     attrs.actual = scope.actual = parseFloat(scope.actual || 0);
-    var radius = Math.min(width, height) / 2.0
-    var arc = d3.svg.arc()
-                .innerRadius(radius * 0.9)
-                .outerRadius(radius)
-                .startAngle(0);
-    var svg = d3.select(element[0]).append('svg')
-                .attr('width', width)
-                .attr('height', height)
-              .append('g')
-                .attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')');
-    var outer = svg.append('path')
-                   .datum({endAngle: 0})
-                   .style('fill', '#8f6')
-                   .attr('d', arc);
-    var arcTween = function arcTween(transition, newAngle) {
-      transition.attrTween('d', function (d) {
-        var interpolate = d3.interpolate(d.endAngle, newAngle);
-        return function (t) {
-          d.endAngle = interpolate(t);
-          return arc(d);
-        };
-      });
-    };
-    outer.transition()
+    attrs.margin = scope.margin = parseFloat(scope.margin || 0);
+
+    var svg = d3.select(element[0]).append('svg');
+
+    var makeTween = function makeTween(arcFunction) {
+      return function (transition, newAngle) {
+        transition.attrTween('d', function (d) {
+          var interpolate = d3.interpolate(d.endAngle, newAngle);
+          return function (t) {
+            d.endAngle = interpolate(t);
+            return arcFunction(d);
+          };
+        });
+      };
+    }
+
+    scope.render = function () {
+      svg.selectAll('*').remove();
+      var width = d3.select(element[0]).node().offsetWidth - scope.margin;
+      var radius = width / 2;
+      var center = 'translate(' + width / 2 + ',' + width / 2  + ')';
+      var arc = d3.svg.arc()
+        .startAngle(0);
+      var arcTween = makeTween(arc);
+      var circle = svg.append('circle')
+        .attr('r', radius * 0.75)
+        .style('fill', '#eee')
+        .attr('transform', center);
+      var inner = svg.append('path')
+        .datum({
+          endAngle: 0,
+          innerRadius: radius * 0.82,
+          outerRadius: radius * 0.86
+        })
+        .style('fill', '#8f6')
+        .attr('transform', center)
+        .attr('d', arc);
+      var outer = svg.append('path')
+        .datum({
+          endAngle: 0,
+          innerRadius: radius * 0.9,
+          outerRadius: radius
+        })
+        .style('fill', '#4d5')
+        .attr('transform', center)
+        .attr('d', arc);
+      outer.transition()
         .duration(750)
         .call(arcTween, scope.actual * 2 * Math.PI);
+      inner.transition()
+        .duration(750)
+        .call(arcTween, scope.expected * 2 * Math.PI);
+    };
+
+    window.onresize = function () {
+      scope.$apply();
+    };
+
+    scope.$watch(function () {
+      return angular.element(window)[0].innerWidth;
+    }, scope.render);
   }
 
   return {
     restrict: 'E',
     scope: {
-      width: '@',
-      height: '@',
       expected: '@',
-      actual: '@'
+      actual: '@',
+      margin: '@'
     },
     link: link
   };
